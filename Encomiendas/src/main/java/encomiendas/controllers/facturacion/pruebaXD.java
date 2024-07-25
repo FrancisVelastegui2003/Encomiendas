935a95862de361e8edcacec15de908b9e5862969
Francis Velastegui <francis.velastegui@epn.edu.ec>
2024-07-25 07:25:09 -0500

    prueba actualizacion

---
+++ Encomiendas/src/main/java/encomiendas/controllers/facturacion/pruebaXD.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package encomiendas.controllers.facturacion;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class pruebaXD {
+    
+    // Puto el que lo lea :p
+    
+}
